eaef2fb12ab1a611245ba2deac4c300cb23307cf
ankitvelani <ankit.velani@gmail.com>
2017-08-11 23:17:23 +0530

    Initial

---
+++ DBConnection.php
@@ -0,0 +1,100 @@
+<?php
+class Connection {
+   // Member Area
+    private $host ,$database,$user,$password;
+    private $stmt;
+    public $conObj;
+            
+    
+    //constructor
+    function __construct()
+    {
+     $this->host="localhost";
+        $this->database="imageratina";
+        $this->user="root";
+        $this->password="root";
+        $this->stmt=NULL;
+
+        try{
+        	
+        	$this->conObj=new PDO("mysql:host=".$this->host.";dbname=".$this->database.";", $this->user, $this->password);
+        }
+        catch (PDOException $e)
+        {
+        	print "Error at Connection !: " . $e->getMessage() . "</br>";
+        }
+
+    }
+    //end of the constructor
+    function updateData($qry)
+    {
+      try{
+        	
+            $this->stmt=  $this->conObj->prepare($qry);   
+            $this->stmt->execute();
+             
+            return 1;
+            
+        }
+        catch (PDOExecption $e){
+            $this->conObj->rollback();
+            return 0;
+        }
+    }
+    
+    
+    function deleteData($qry)
+    {
+
+      
+        try{
+        	
+            $this->stmt=  $this->conObj->prepare($qry);
+            $this->stmt->execute();
+              
+            return 1;
+            
+        }
+        catch (PDOExecption $e){
+            $this->conObj->rollback();
+            return 0;
+        }
+    }
+    
+    //insert data function
+    function putData($qry)
+    {
+      try{
+            $this->stmt=  $this->conObj->prepare($qry);
+            $this->stmt->execute();
+            return $this->conObj->lastInsertId();
+            
+        }
+        catch (PDOExecption $e){
+            $this->conObj->rollback();
+            return 0;
+        }
+    }
+    //end of the data insert function 
+    
+    
+    //retrive data function
+    public function getData($qry)
+    {
+        $this->stmt= $this->conObj->prepare($qry);
+        $this->stmt->execute();
+        
+        $row=$this->stmt->fetchAll();
+        return $row;
+ 
+    }
+    // retruve data function end        
+    
+    
+    function __destruct() {
+       // echo "Desctructed ..";
+       $this->conObj=NULL;
+    }
+}
+
+?>
